17e8fac8cb7703102a33b9f3bcf2a076cd6574c4
João Victor <joaovictorfernandescastro@gmail.com>
2025-07-09 00:45:07 -0300

    Corrigindo: adicionando pasta web como diretório normal

---
+++ web/src/main.tsx
@@ -0,0 +1,11 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: mandatory by React */
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { App } from './app.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
